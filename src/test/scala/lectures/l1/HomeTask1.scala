57f7460092961f0df82f8d2cb20fbf0dd3d04cf8
Greg Pevnev <gpevnev@gmail.com>
2018-03-01 18:31:57 +0300

    added first homework

--- src/test/scala/lectures/l1/HomeTask1.scala
+++ src/test/scala/lectures/l1/HomeTask1.scala
@@ -2,35 +2,39 @@ package lectures.l1
 
 import org.scalatest.{FlatSpec, Matchers}
 
+// sbt server - incremantal compilation??
+
 object CountRoundToLower {
   def undefined() = throw new IllegalArgumentException("*.5")
 
-  //  def toHigh(v) = {
-  //    val number = v * 10 % 10
-  //
-  //  }
+  def toHigh(v: Double): Int = (v * 10 % 10).toInt
 
-  //  def counter(toUp) = ???
+  def counter(fractionalPart: Int): Int =
+    fractionalPart match {
+      case 5          => undefined()
+      case n if n < 5 => 1
+      case _          => 0
+    }
 
-  //  def count(list) = list.map(v => counter(toHigh(v))).sum
+  def count(list: Seq[Double]): Int = list.map(v => counter(toHigh(v))).sum
 }
 
 class CountRoundToLowerTest extends FlatSpec with Matchers {
 
-  //  import CountRoundToLower.count
+  import CountRoundToLower.count
 
   it should "count numbers" in {
-    //    count(Seq(1, 2l, 1.1f)) shouldBe 3
-    //    count(Seq(1.3f, 1.6f)) shouldBe 1
+    count(Seq(1, 2l, 1.1f)) shouldBe 3
+    count(Seq(1.3f, 1.6f)) shouldBe 1
   }
 
   it should "throw error" in {
     assertThrows[IllegalArgumentException](
-      //      count(Seq(1, 1.6f, 1.5f))
+      count(Seq(1, 1.6f, 1.5f))
     )
   }
 
   it should "support big long number" in {
-    //    count(Seq(1000000000l)) shouldBe 1
+    count(Seq(1000000000l)) shouldBe 1
   }
 }
